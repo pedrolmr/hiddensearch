da1511be490185a0e500f56ff4286147bb4444ec
pedrolmr <pedroluismontesinosrivera@gmail.com>
2022-02-20 02:35:38 -0500

    can now toggle search button

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-script.js;
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
